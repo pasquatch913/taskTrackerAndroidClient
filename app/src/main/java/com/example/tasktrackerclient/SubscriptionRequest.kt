97bf21a1fa03ab8eb9fbdcc34f39306b7ac93c17
Paschall Davis <paschall913@gmail.com>
2019-07-29 20:30:25 -0400

    client variable names must match server variable names to allow marshalling json

--- app/src/main/java/com/example/tasktrackerclient/SubscriptionRequest.kt
+++ app/src/main/java/com/example/tasktrackerclient/SubscriptionRequest.kt
@@ -2,9 +2,8 @@ package com.example.tasktrackerclient
 
 class SubscriptionRequest(
 
-    val id: Int,
     val name: String,
-    val completionsGoal: Int,
+    val necessaryCompletions: Int,
     val weight: Int,
     val period: TaskPeriod,
     val active: Boolean = true

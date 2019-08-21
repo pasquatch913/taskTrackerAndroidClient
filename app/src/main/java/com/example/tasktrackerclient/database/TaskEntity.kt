d53aa174cc2eb956a284b246a85542391e39d7cc
Paschall Davis <paschall913@gmail.com>
2019-08-21 11:01:29 -0400

    enhance task entity object and upgrade table version

--- app/src/main/java/com/example/tasktrackerclient/database/TaskEntity.kt
+++ app/src/main/java/com/example/tasktrackerclient/database/TaskEntity.kt
@@ -7,15 +7,14 @@ import java.time.LocalDate
 
 @Entity(tableName = "task_table")
 data class TaskEntity(
-//    @PrimaryKey(autoGenerate = true)
-//    val localId: Int,
+
     @PrimaryKey
     val id: Int,
-    val name: String
-//    val completionsGoal: Int,
-//    val completions: Int,
-//    val weight: Int,
-//    val dueDate: LocalDate,
-//    val active: Boolean,
-//    val recurring: Boolean
+    val name: String,
+    val completionsGoal: Int,
+    val completions: Int,
+    val weight: Int,
+    val dueDate: LocalDate,
+    val active: Boolean,
+    val recurring: Boolean
 )

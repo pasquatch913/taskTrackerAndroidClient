c57bf9e0e1c1cbe01a4a472541fb1844f9a29c72
Paschall Davis <paschall913@gmail.com>
2019-08-29 08:07:12 -0400

    wrap requests to server in try/catch for timeoutexceptions

--- app/src/main/java/com/example/tasktrackerclient/rest/RestService.kt
+++ app/src/main/java/com/example/tasktrackerclient/rest/RestService.kt
@@ -13,6 +13,7 @@ import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.launch
+import java.net.SocketTimeoutException
 import kotlin.coroutines.CoroutineContext
 
 class RestService(val context: Context) : CoroutineScope {
@@ -35,45 +36,53 @@ class RestService(val context: Context) : CoroutineScope {
         appWidgetIds: IntArray
     ) {
         launch {
-            val response =
-                webRequestService.updateTaskCompletions(taskId, newCompletions).await()
-            println(response)
-            if (response.isSuccessful) {
-                println(response.code())
-                val whereClause = "${DbHelper.COLUMN_ID} = ?"
-                val whereArgs = arrayOf(taskId.toString())
-                val newValue = ContentValues()
-                newValue.put(DbHelper.COLUMN_COMPS, newCompletions)
-                context.contentResolver.update(uri, newValue, whereClause, whereArgs)
-                //notify that dataset is changed
-                appWidgetManager.notifyAppWidgetViewDataChanged(appWidgetIds, R.id.widgetCollectionList)
-            } else {
-                println("failed to execute request")
-                println("call: " + response)
+            try {
+                val response =
+                    webRequestService.updateTaskCompletions(taskId, newCompletions).await()
+                println(response)
+                if (response.isSuccessful) {
+                    println(response.code())
+                    val whereClause = "${DbHelper.COLUMN_ID} = ?"
+                    val whereArgs = arrayOf(taskId.toString())
+                    val newValue = ContentValues()
+                    newValue.put(DbHelper.COLUMN_COMPS, newCompletions)
+                    context.contentResolver.update(uri, newValue, whereClause, whereArgs)
+                    //notify that dataset is changed
+                    appWidgetManager.notifyAppWidgetViewDataChanged(appWidgetIds, R.id.widgetCollectionList)
+                } else {
+                    println("failed to execute request")
+                    println("call: " + response)
+                }
+            } catch (ex: SocketTimeoutException) {
+                Log.d("RestService", "request to update remote server timed out after 10s!")
             }
         }
     }
 
     fun updateAllTasks(appWidgetManager: AppWidgetManager, appWidgetIds: IntArray) {
         launch {
-            val response =
-                webRequestService.fetchAllActiveTasks().await()
-            println(response)
-            if (response.isSuccessful) {
-                println(response.code())
-                val deleted = context.contentResolver.delete(uri, null, null)
-                Log.d("Rows deleted", deleted.toString())
-                val responseList = ArrayList<TaskDTO>()
-                response.body()!!.map(mapper::taskInstanceResponseToTaskDto)
-                    .map(responseList::add)
+            try {
+                val response =
+                    webRequestService.fetchAllActiveTasks().await()
+                println(response)
+                if (response.isSuccessful) {
+                    println(response.code())
+                    val deleted = context.contentResolver.delete(uri, null, null)
+                    Log.d("Rows deleted", deleted.toString())
+                    val responseList = ArrayList<TaskDTO>()
+                    response.body()!!.map(mapper::taskInstanceResponseToTaskDto)
+                        .map(responseList::add)
 
-                responseList.map(mapper::taskDtoToTaskEntity)
-                    .map(mapper::taskEntityToContentValues)
-                    .map { n -> context.contentResolver.insert(uri, n) }
-                appWidgetManager.notifyAppWidgetViewDataChanged(appWidgetIds, R.id.widgetCollectionList)
-            } else {
-                println("failed to execute request")
-                println("call: " + response)
+                    responseList.map(mapper::taskDtoToTaskEntity)
+                        .map(mapper::taskEntityToContentValues)
+                        .map { n -> context.contentResolver.insert(uri, n) }
+                    appWidgetManager.notifyAppWidgetViewDataChanged(appWidgetIds, R.id.widgetCollectionList)
+                } else {
+                    println("failed to execute request")
+                    println("call: " + response)
+                }
+            } catch (ex: SocketTimeoutException) {
+                Log.d("RestService", "request to pull from remote server timed out after 10s!")
             }
         }
     }
